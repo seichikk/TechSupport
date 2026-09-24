ca536beb9edc85df116ee5822bb3903a878a49f9
seichikk <trchfk@gmail.com>
2026-09-24 13:04:54 +0300

    добавление новых действий в тест

--- Program.cs
+++ Program.cs
@@ -6,16 +6,21 @@ public class Program
 
         incident.Resolve("перезагрузили принтер");
         incident.ChangePriority(4);
+        incident.ChangePriority(4);
         incident.Assign("Петров");
         incident.ChangePriority(5);
+        incident.Assign("Сидоров");
         incident.Assign("Сидоров");
         incident.RequestInfo("какая модель принтера?");
         incident.Resolve("обновили драйвер");
         incident.Escalate("пользователь не отвечает два дня");
+        incident.Escalate("пользователь не отвечает уже три дня");
         incident.ProvideInfo("HP LaserJet 1020");
         incident.Resolve("обновили драйвер");
         incident.Reopen("принтер снова не печатает");
+        incident.Reopen("принтер всё ещё не печатает");
         incident.Resolve("заменили картридж");
+        incident.Close();
         incident.Close();
         incident.Assign("Иванов");
 
